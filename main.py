dfac7da21a5f6556ee128024fee5271130dbd549
Creampanda <andrewfromtokyo@gmail.com>
2021-04-12 21:14:23 +0300

    gettin rain forecast for 12 hours

--- main.py
+++ main.py
@@ -7,8 +7,16 @@ owm_endpoint = "https://api.openweathermap.org/data/2.5/onecall"
 weather_params = {
     "lat": "56.331718",
     "lon": "36.729210",
-    "appid": api_key
+    "appid": api_key,
+    "exclude": "current,minutely,daily"
 }
 
 response = requests.get(owm_endpoint,params=weather_params)
-print(response)
+response.raise_for_status()
+weather_data = response.json()
+
+will_rain = False
+
+for hour in weather_data["hourly"][:12]:
+    if hour["weather"][0]["id"] < 700:
+        will_rain
