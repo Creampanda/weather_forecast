58d6cf3f321ff1c64afca4b254d1ca66c9b9d6f6
Creampanda <andrewfromtokyo@gmail.com>
2021-04-14 00:29:42 +0300

    making with crontab instead of while loop

--- main.py
+++ main.py
@@ -15,41 +15,40 @@ weather_params = {
     "exclude": "current,minutely"
 }
 
-send_time = datetime.time(7,0,0,000000)
+# send_time = datetime.time(7,0,0,000000)
 
-while(1):
-    if datetime.datetime.now().time() == send_time:
-        will_rain = False
-        counter = 0
 
-        response = requests.get(owm_endpoint,params=weather_params)
-        response.raise_for_status()
-        weather_data = response.json()
+will_rain = False
+counter = 0
 
-        for hour in weather_data["hourly"][:24]:
-            counter += 1
-            if hour["weather"][0]["id"] < 700:
-                will_rain = True
-                rain_hour = (send_time.hour + counter) % 24
-                if rain_hour < 10:
-                    rain_hour = f"0{rain_hour}.00"
-                else:
-                    rain_hour = f"{rain_hour}.00"
+response = requests.get(owm_endpoint,params=weather_params)
+response.raise_for_status()
+weather_data = response.json()
 
-        day_temp = round(weather_data["daily"][0]["temp"]["day"] - 273)
-        night_temp = round(weather_data["daily"][0]["temp"]["night"] - 273)
-        if will_rain:
-            weather_message = f"""
-                Зеленоград
-                Температура днем: {day_temp}°C
-                Температура ночью: {night_temp}°C
-                Ожидается дождь c {rain_hour}
-                """
+for hour in weather_data["hourly"][:24]:
+    counter += 1
+    if hour["weather"][0]["id"] < 700:
+        will_rain = True
+        rain_hour = (7 + counter) % 24
+        if rain_hour < 10:
+            rain_hour = f"0{rain_hour}.00"
         else:
-            weather_message = f"""
-                Зеленоград
-                Температура днем: {day_temp}°C
-                Температура ночью: {night_temp}°C
-                Дождь не ожидается
-                """
-        bot.send_message(config.chat_id, weather_message)
+            rain_hour = f"{rain_hour}.00"
+
+day_temp = round(weather_data["daily"][0]["temp"]["day"] - 273)
+night_temp = round(weather_data["daily"][0]["temp"]["night"] - 273)
+if will_rain:
+    weather_message = f"""
+        Зеленоград
+        Температура днем: {day_temp}°C
+        Температура ночью: {night_temp}°C
+        Ожидается дождь c {rain_hour}
+        """
+else:
+    weather_message = f"""
+        Зеленоград
+        Температура днем: {day_temp}°C
+        Температура ночью: {night_temp}°C
+        Дождь не ожидается
+        """
+bot.send_message(config.chat_id, weather_message)
